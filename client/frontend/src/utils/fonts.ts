e1d53258127f7a32baadc2b1eee5f98b71f730bd
Aditya kumar <98655260+164adityakumar@users.noreply.github.com>
2024-04-18 01:16:26 +0530

    frontend setup

---
+++ client/frontend/src/utils/fonts.ts
@@ -0,0 +1,9 @@
+import { Dancing_Script } from 'next/font/google';
+import { Josefin_Sans } from 'next/font/google';
+import { Poppins } from 'next/font/google';
+export const dancingScript = Dancing_Script({ subsets: ['latin'] });
+export const JosefinSans = Josefin_Sans({ subsets: ['latin'] });
+export const poppins = Poppins({
+    subsets: ['latin'],
+    weight: '500'
+});
