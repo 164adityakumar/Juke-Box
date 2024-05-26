535409551da7f20a2aefcb78b72a487bd59c8f50
Aditya kumar <98655260+164adityakumar@users.noreply.github.com>
2024-05-26 18:56:25 +0530

    refactor: Update CORS configuration to allow all origins

--- server/src/routes/api/rooms.ts
+++ server/src/routes/api/rooms.ts
@@ -1,7 +1,11 @@
 const roomrouter=require('express').Router()
 const { handleJoin, handleClose } = require("../../ws/messageHandlers");
 const { RedisSubscriptionManager } = require("../../redis");
+const cors = require('cors');
 
+roomrouter.use(cors({
+    origin: '*', // replace with your client's origin in production
+  }));
 // Route for creating a room
 roomrouter.post('/create', (req, res) => {
     const roomId = req.body.roomId;
@@ -12,7 +16,7 @@ roomrouter.post('/create', (req, res) => {
 roomrouter.get('/:roomId/getQueue', (req, res) => {
     const roomId = req.params.roomId;
     const queue = RedisSubscriptionManager.getInstance().getSongQueue(roomId);
-    res.send(queue);
+    res.send(queue    );
 });
 
 module.exports = roomrouter;
