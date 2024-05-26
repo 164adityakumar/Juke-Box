77fa7007c5a6610d59725b5529226064d7d7ebf0
Aditya kumar <98655260+164adityakumar@users.noreply.github.com>
2024-05-26 18:45:24 +0530

    refactor: Update CORS configuration to allow specific origins

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,22 +20,30 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
-const allowCrossDomain = (req, res, next) => {
-    res.header(`Access-Control-Allow-Origin`, `example.com`);
-    res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
-    res.header(`Access-Control-Allow-Headers`, `Content-Type`);
-    next();
-  };
+// const allowCrossDomain = (req, res, next) => {
+//     res.header(`Access-Control-Allow-Origin`, `example.com`);
+//     res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+//     res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+//     next();
+//   };
 
 app.use(routes);
-app.use(allowCrossDomain);
-app.use(cors({
-    origin: '*',
-    allowedHeaders: ['Access-Control-Allow-Origin'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-  }));
+// app.use(allowCrossDomain);
+const allowedOrigins = ["https://juke-box-web.vercel.app"];
 
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+  },
+  methods: 'GET,POST,PUT,DELETE,HEAD,PATCH',
+  allowedHeaders: 'Access-Control-Allow-Origin',
+  credentials: true,
+  
+}));
 const wss = new WebSocketServer({ server });
 
 export const users: { [key: string]: {
